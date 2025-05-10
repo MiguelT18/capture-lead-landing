ff932d47610c611c2c6988a5887b25a031e6c0fd
Miguel Teran <miguel@cyberjunk.>
2025-05-10 04:00:01 -0400

    finish frontend

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# Tareas pendientes
+
+1. [❓] Crear un modal después de enviar los datos del formulario principal que contenga los datos del formulario del modal secundario opcional para capturar como una descripción "¿Qué opinas de la programación?"
+
+---
+
+# Brainstorming
+
+- Utilizar ML para analizar el sentimiento de las respuestas del input "¿Qué opinas de la programación?" para luego mediante estadísticas ver cuántas personas de las que se registraron y se unieron al grupo de Telegram realmente están interesadas.
---
+++ src/assets/icons.js
@@ -0,0 +1,37 @@
+import BookIcon from "@assets/icons/Book.astro"
+import ChatIcon from "@assets/icons/Chat.astro"
+import CodeIcon from "@assets/icons/Code.astro"
+import MedalIcon from "@assets/icons/Medal.astro"
+import ThunderIcon from "@assets/icons/Thunder.astro"
+import CommunityIcon from "@assets/icons/Community.astro"
+import Warning from "./icons/Warning.astro"
+import CheckCircle from "./icons/CheckCircle.astro"
+import ShieldCheck from "./icons/ShieldCheck.astro"
+import LightBulb from "./icons/LightBulb.astro"
+import Rocket from "./icons/Rocket.astro"
+import Hearth from "./icons/Hearth.astro"
+
+export const BenefitIcons = {
+  book: BookIcon,
+  chat: ChatIcon,
+  code: CodeIcon,
+  medal: MedalIcon,
+  thunder: ThunderIcon,
+  community: CommunityIcon,
+}
+
+export const RuleIcons = {
+  warning: Warning,
+  check: CheckCircle,
+  shielCheck: ShieldCheck
+}
+
+export const TargetIcons = {
+  code: CodeIcon,
+  lightbulb: LightBulb,
+  rocket: Rocket
+}
+
+export const FooterIcons = {
+  hearth: Hearth
+}
