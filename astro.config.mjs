414704a465f3d5d56ed8c47a1d4c63ac7ee4cbd8
Miguel Teran <miguel@cyberjunk.>
2025-07-05 17:04:34 -0400

    fix: eliminé la configuración de astrobd ya que no estaba siendo usada

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
-import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
